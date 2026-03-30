38444039f05e1571fc9829eb6903d135cac7b921
JoseBoyLoveMaker <emanuel.bernardo.c@gmail.com>
2026-03-30 17:19:16 -0300

    Adicionando painel web estático

--- API/Program.cs
+++ API/Program.cs
@@ -26,7 +26,11 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Painel", policy =>
     {
         policy
-            .WithOrigins("https://localhost:7296")
+            .WithOrigins(
+                "http://127.0.0.1:5500",
+                "http://localhost:5500",
+                "https://localhost:7296"
+            )
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
